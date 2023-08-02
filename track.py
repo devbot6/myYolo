5433ef7200d5e56e71cf4ab469521f745f9c02f6
devonLikesJava <devhulse04@icloud.com>
2023-08-02 17:21:00 -0400

    vid changed

--- track.py
+++ track.py
@@ -6,5 +6,5 @@ model = YOLO('yolov8n-seg.pt')  # load an official segmentation model
 
 
 # Track with the model
-results = model.track(source="https://youtu.be/nkOk_P5VnOA", show=True)
-results = model.track(source="https://youtu.be/nkOk_P5VnOA", show=True, tracker="bytetrack.yaml")
+results = model.track(source="https://youtu.be/13OtZFWdhwQ", show=True)
+results = model.track(source="https://youtu.be/13OtZFWdhwQ", show=True, tracker="bytetrack.yaml")
